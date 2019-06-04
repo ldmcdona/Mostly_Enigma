bb3b20aac4d09d909e6857c572a0d421b6fb200d
ldmcdona <ldmcdona@ualberta.ca>
2019-06-03 23:07:47 -0600

    I think this is the right file

---
+++ enigma.py
@@ -0,0 +1,244 @@
+import random
+
+class Enigma:
+    def __init__(self, rotor1_f, rotor1_b, rotor2_f, rotor2_b, rotor3_f, rotor3_b, plugs):
+        self.rotor1_f = rotor1_f
+        self.rotor1_b = rotor1_b
+        self.rotor2_f = rotor2_f
+        self.rotor2_b = rotor2_b
+        self.rotor3_f = rotor3_f
+        self.rotor3_b = rotor3_b
+        self.plugs = plugs
+        self.offset1 = 0
+        self.offset2 = 0
+        self.offset3 = 0
+        self.master  = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+    def encode(self):
+        output = ""
+        print("Enter message.")
+        x = input(">")
+        x = x.lower()
+
+        for letter in x:
+            if letter in self.master:
+
+                in_i1 = self.master.index(letter)
+
+                if in_i1 in self.plugs:
+                    in_i1 = self.plugs[in_i1]
+
+                in_i1 += self.offset1
+                if in_i1 > 25:
+                    in_i1 -= 26
+                in_o1 = self.rotor1_f[in_i1]
+                in_o1 -= self.offset1
+                if in_o1 < 0:
+                    in_o1 += 26
+
+                in_i2 = in_o1
+                in_i2 += self.offset2
+                if in_i2 > 25:
+                    in_i2 -= 26
+                in_o2 = self.rotor2_f[in_i2]
+                in_o2 -= self.offset2
+                if in_o2 < 0:
+                    in_o2 += 26
+
+                in_i3 = in_o2
+                in_i3 += self.offset3
+                if in_i3 > 25:
+                    in_i3 -= 26
+                in_o3 = self.rotor3_f[in_i3]
+                in_o3 -= self.offset3
+                if in_o3 < 0:
+                    in_o3 += 26
+
+                out_i3 = in_o3 + 13
+                if out_i3 > 25:
+                    out_i3 -= 26
+
+                out_i3 += self.offset3
+                if out_i3 > 25:
+                    out_i3 -= 26
+                out_o3 = self.rotor3_b[out_i3]
+                out_o3 -= self.offset3
+                if out_o3 < 0:
+                    out_o3 += 26
+
+                out_i2 = out_o3
+                out_i2 += self.offset2
+                if out_i2 > 25:
+                    out_i2 -= 26
+                out_o2 = self.rotor2_b[out_i2]
+                out_o2 -= self.offset2
+                if out_o2 < 0:
+                    out_o2 += 26
+
+                out_i1 = out_o2
+                out_i1 += self.offset1
+                if out_i1 > 25:
+                    out_i1 -= 26
+                out_o1 = self.rotor1_b[out_i1]
+                out_o1 -= self.offset1
+                if out_o1 < 0:
+                    out_o1 += 26
+
+                self.offset1 += 1
+                if self.offset1 > 25:
+                    self.offset1 -= 26
+                    self.offset2 += 1
+                if self.offset2 > 25:
+                    self.offset2 -= 26
+                    self.offset3 += 1
+                if self.offset3 > 25:
+                    self.offset3 -= 26
+
+                if out_o1 in self.plugs:
+                    out_o1 = self.plugs[out_o1]
+
+                final = self.master[out_o1]
+
+                output += final
+
+            else:
+                output += letter
+        
+        print("Your encoded message: " + output)
+        return
+
+    def modify(self):
+        print("Change Plugs? (y/n)")
+        if input(">") == "y":
+            while True:
+                self.plugs = {}
+                taken = []
+                print("Enter the number of plugs you want to use. (0 to 10)")
+                pnum = input(">")
+                pnum = int(pnum)
+                if pnum > 10 or pnum < 0:
+                    print("Invalid number entered.")
+                else:
+                    break
+            for i in range(pnum):
+                while True:
+                    print("Enter first letter in pair. (A-Z)")
+                    l1 = input(">")
+                    l1 = l1.lower()
+                    print("Enter second letter in pair. (A-Z)")
+                    l2 = input(">")
+                    l2 = l2.lower()
+                    if (ord(l1) > 96 and ord(l1) < 123) and (ord(l2) > 96 and ord(l2) < 123):
+                        check = False
+                        if l1 in taken or l2 in taken:
+                            check = True
+                        if check == False:
+                            taken.append(l1)
+                            taken.append(l2)
+                            self.plugs[l1] = l2
+                            self.plugs[l2] = l1
+                            break
+                        else:
+                            print("Invalid character entered. Character already used.")
+                    else:
+                        print("Invalid character entered. Must be a letter.")
+
+        print("Swap rotors? (y/n)")
+        if input(">") == "y":
+            self.rotor1_f, self.rotor1_b, self.rotor2_f, self.rotor2_b, self.rotor3_f, self.rotor3_b = rotorWheels()
+
+        print("Set rotor offset? (y/n)")
+        if input(">") == "y":
+            i = 0
+            off = []
+            while i < 3:
+                print("Enter offset for rotor", i, "(0 to 25)")
+                x = input(">")
+                x = int(x)
+                if x >= 0 and x <= 25:
+                    off.append(x)
+                    i += 1
+                else:
+                    print("Invalid number.")
+            self.offset1 = off[0]
+            self.offset2 = off[1]
+            self.offset3 = off[2]
+
+        print("Modification complete.")
+        return 
+
+
+def main():
+    r1f, r1b, r2f, r2b, r3f, r3b = rotorWheels()
+    p = {}
+    box = Enigma(r1f, r1b, r2f, r2b, r3f, r3b, p)
+    while True:
+        print("Enigma Machine. Enter E to Encode. Enter M to Modify. Enter Q to Quit.")
+        answer1 = input(">")
+        if answer1 == "E":
+            box.encode()
+        elif answer1 == "M":
+            box.modify()
+        elif answer1 == "Q":
+            break
+        else:
+            print("Invalid Input.")
+
+
+def convert(r):
+    master = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    r1_f = {}
+    r1_b = {}
+    
+    i = 0
+    while i < 26:
+        a = r[i]
+        b = master.index(a)
+        r1_f[i] = b
+        r1_b[b] = i
+        i += 1
+    
+    return r1_f, r1_b
+
+
+def rotorWheels():
+    w1 = "PEZUOHXSCVFMTBGLRINQJWAYDK"
+    w2 = "ZOUESYDKFWPCIQXHMVBLGNJRAT"
+    w3 = "EHRVXGAOBQUSIMZFLYNWKTPDJC"
+    w4 = "IMETCGFRAYSQBZXWLHKDVUPOJN"
+    w5 = "QWERTZUIOASDFGHJKPYXCVBNML"
+
+    w1 = w1.lower()
+    w2 = w2.lower()
+    w3 = w3.lower()
+    w4 = w4.lower()
+    w5 = w5.lower()
+
+    pile = [w1, w2, w3, w4, w5]
+
+    r_f = []
+    r_b = []
+
+    for item in pile:
+        f, b = convert(item)
+        r_f.append(f)
+        r_b.append(b)
+
+    taken = []
+    i = 0
+    while i < 3:
+        print("Choose which rotor to use for position", i, "(1 to 5)")
+        x = input(">")
+        x = int(x)
+        if x not in taken:
+            if x < 6 and x > 0:
+                taken.append(x)
+                i += 1
+            else:
+                print("Invalid number.")
+        else:
+            print("Invalid input.")
+
+    return r_f[taken[0]-1], r_b[taken[0]-1], r_f[taken[1]-1], r_b[taken[1]-1], r_f[taken[2]-1], r_b[taken[2]-1]
+
+main()
